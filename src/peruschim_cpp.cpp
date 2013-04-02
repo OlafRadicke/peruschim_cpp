f799399dd82c5483cb32a0c2db9a58f283aef415
Olaf Radicke <briefkasten@olaf-radicke.de>
2013-04-02 23:26:37 +0200

    missing file.

---
+++ src/peruschim_cpp.cpp
@@ -0,0 +1,31 @@
+#include <tnt/tntnet.h>
+#include <string>
+
+#include "./model/Config.h"
+
+int main ( int argc, char* argv[] )
+{
+    Config config;
+    int port =  atoi(config.get( "APP-PORT" ).c_str());
+    std::string ip_addr =  config.get( "APP-IP" );
+
+    
+    try
+    {
+        tnt::Tntnet app;
+        app.listen( ip_addr, port );
+        app.mapUrl( "^/login", "login" ).setPathInfo( "login" );
+        app.mapUrl( "^/home", "home" ).setPathInfo( "home" );
+        app.mapUrl( "^/newaccount", "newaccount" ).setPathInfo( "newaccount" );
+        app.mapUrl( "^/newquote", "newquote" ).setPathInfo( "newquote" );
+        app.mapUrl( "^/$", "home" ).setPathInfo( "home" );
+        app.mapUrl( "^/(.*)$", "$1" );
+        
+        std::cout << "peruschim cpp is started and run on http://" <<  ip_addr \
+            << ":" <<  port << "/" << std::endl;
+        app.run();
+    } catch ( const std::exception& e )
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
