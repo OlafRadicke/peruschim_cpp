d05cdc3dae60c269cf567138b0db43a25c554225
Olaf Radicke <briefkasten@olaf-radicke.de>
2013-07-21 12:09:02 +0200

    Begin with change from name global to shared.

--- src/controller/EditAccountController.cpp
+++ src/controller/EditAccountController.cpp
@@ -29,11 +29,14 @@
 #include "models/QuoteRegister.h"
 #include "models/OString.h"
 #include <iostream>
+#include <cxxtools/log.h>
 
 
 
 # define ERROR std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] " <<
 # define DEBUG std::cout << "[" << __FILE__ << ":" << __LINE__ << "] " <<
+
+log_define("component.EditAccountController")
 
 class EditAccountController : public tnt::Component
 {
@@ -59,6 +62,12 @@ unsigned EditAccountController::operator() (tnt::HttpRequest& request, tnt::Http
     TNT_SESSION_GLOBAL_VAR( AccountData,              g_accountData, () );
     TNT_SESSION_GLOBAL_VAR( std::vector<std::string>, g_userRolls, () );
     TNT_SESSION_GLOBAL_VAR( std::vector<std::string>, g_allRolls, () );
+    TNT_SESSION_SHARED_VAR( bool,                     s_isEditAccount, () );
+
+    // ACL Check
+    if ( userSession.isInRole ( "admin" ) == false ) {
+        return reply.redirect ( "/access_denied" );
+    };
 
 
     // URL arguments
@@ -76,63 +85,68 @@ unsigned EditAccountController::operator() (tnt::HttpRequest& request, tnt::Http
         qparam.args<std::string>("args_userroles");
     bool arg_is_inactive =
         qparam.arg<bool>("arg_is_inactive");
-    std::string arg_button_update_account =
-        qparam.arg<std::string>("arg_button_update_account");
+    bool arg_update_account_button =
+        qparam.arg<bool>("arg_update_account_button");
+
     unsigned long arg_edit_account_id =
         qparam.arg<unsigned long>("arg_edit_account_id");
+    bool arg_edit_account_button =
+        qparam.arg<bool>("arg_edit_account_button");
+    s_isEditAccount = arg_edit_account_button;
+
     unsigned long arg_delete_account_id =
         qparam.arg<unsigned long>("arg_delete_account_id");
+    bool arg_delete_account_button =
+        qparam.arg<bool>("arg_delete_account_button");
+
     unsigned long arg_affirmation_delete_account_id =
         qparam.arg<unsigned long>("arg_affirmation_delete_account_id");
+    bool arg_affirmation_delete_button =
+        qparam.arg<bool>("arg_affirmation_delete_button");
 
-
-    // ACL Check
-    if ( userSession.isInRole ( "admin" ) == false ) {
-        return reply.redirect ( "/access_denied" );
-    };
 
     g_accountList = WebACL::getAllAccounts();
-    DEBUG "g_accountList.size(): " << g_accountList.size() << std::endl;
+    log_debug( "g_accountList.size(): " << g_accountList.size()  );
 
     // if a account selected for editing?
-    DEBUG "arg_edit_account_id: " << arg_edit_account_id << std::endl;
-    if ( arg_edit_account_id > 0 ) {
+    log_debug( "arg_edit_account_id: " << arg_edit_account_id );
+    if ( arg_edit_account_button ) {
         g_affirmation = "";
         g_feedback = "";
         g_accountData =  WebACL::getAccountsWithID ( arg_edit_account_id );
-        DEBUG "g_accountData.getLogin_name(): " <<       
-            g_accountData.getLogin_name() << std::endl;
+        log_debug( "g_accountData.getLogin_name(): " <<
+            g_accountData.getLogin_name()  );
         g_userRolls = WebACL::getRoll ( g_accountData.getLogin_name() );
         g_allRolls = WebACL::getAllRolls();
         g_session_account_id = arg_edit_account_id;
-        DEBUG "g_accountData.getAccount_disable(): " <<       
-            g_accountData.getAccount_disable() << std::endl;
+        log_debug( "g_accountData.getAccount_disable(): " <<
+            g_accountData.getAccount_disable() );
 
     }
 
     // is button account data update pushed?
-    if ( arg_button_update_account == "Speichern" ) {
+    if ( arg_update_account_button ) {
         g_affirmation = "";
         g_feedback = "";
-        DEBUG "Speichern" << std::endl;
-        DEBUG "g_session_account_id: " << g_session_account_id << std::endl;
+        log_debug( "Speichern" );
+        log_debug( "g_session_account_id: " << g_session_account_id );
         g_accountData.setID( g_session_account_id );
-        DEBUG "arg_login_name: " << arg_login_name << std::endl;
+        log_debug( "arg_login_name: " << arg_login_name );
         g_accountData.setLogin_name( arg_login_name );
-        DEBUG "arg_name: " << arg_name << std::endl;
+        log_debug( "arg_name: " << arg_name );
         g_accountData.setReal_name( arg_name );
-        DEBUG "arg_mail: " << arg_mail << std::endl;
+        log_debug( "arg_mail: " << arg_mail );
         g_accountData.setEmail( arg_mail );
-        DEBUG "arg_is_inactive: " << arg_is_inactive << std::endl;
+        log_debug( "arg_is_inactive: " << arg_is_inactive );
         g_accountData.setAccount_disable( arg_is_inactive );
 
-        DEBUG "args_userroles.size(): " << args_userroles.size() << std::endl;
+        log_debug( "args_userroles.size(): " << args_userroles.size() );
         WebACL::reSetUserRolls( g_session_account_id, args_userroles);
         g_accountData.saveUpdate();
 
         // check if new password set.
         if (arg_password_a != "" ) {
-            DEBUG "arg_password_a" << arg_password_a << std::endl;
+            log_debug( "arg_password_a" << arg_password_a );
             // check equal of new password.
             if ( arg_password_a != arg_password_b ) {
                 g_feedback = "Das Passwort ist nicht gleich!";
@@ -150,13 +164,13 @@ unsigned EditAccountController::operator() (tnt::HttpRequest& request, tnt::Http
     }
 
     // is button delete account pushed?
-    if ( arg_delete_account_id > 0 ) {
+    if ( arg_delete_account_button ) {
         g_affirmation = "";
         g_feedback = "";
-        g_session_account_id = arg_delete_account_id;        
+        g_session_account_id = arg_delete_account_id;
         g_accountData =  WebACL::getAccountsWithID ( g_session_account_id );
-        DEBUG "g_accountData.getLogin_name(): " <<       
-            g_accountData.getLogin_name() << std::endl;
+        log_debug( "g_accountData.getLogin_name(): " <<
+            g_accountData.getLogin_name() );
         g_affirmation = "Account \""+ g_accountData.getLogin_name() +" (ID " + \
             cxxtools::convert<std::string>( g_session_account_id ) \
             + ")\" und allen zugehörigen Daten wirklich löschen?";
@@ -164,10 +178,10 @@ unsigned EditAccountController::operator() (tnt::HttpRequest& request, tnt::Http
     }
 
     // if delete affirmation clicked.
-    if ( arg_affirmation_delete_account_id > 0 ) {
+    if ( arg_affirmation_delete_button ) {
         g_affirmation = "";
         g_feedback = "";
-        DEBUG "will löschen: " << arg_affirmation_delete_account_id << std::endl;
+        log_debug( "will löschen: " << arg_affirmation_delete_account_id );
         g_accountData.setID( arg_affirmation_delete_account_id );
         g_accountData.deleteAllData();
         g_feedback = "Der Account mit der Id " + \
